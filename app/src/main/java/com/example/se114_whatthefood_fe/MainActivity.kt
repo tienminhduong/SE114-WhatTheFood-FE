24b365c91e82346d1659cdff81d981b9cbdabf2e
LuNguyenDuyDuc <duclnd.t1.2023@gmail.com>
2025-06-13 10:45:04 +0700

    test

--- app/src/main/java/com/example/se114_whatthefood_fe/MainActivity.kt
+++ app/src/main/java/com/example/se114_whatthefood_fe/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.se114_whatthefood_fe
 
+import android.R.attr.text
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.se114_whatthefood_fe.ui.theme.SE114WhatTheFoodFETheme
 
 class MainActivity : ComponentActivity() {
@@ -36,6 +40,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+
+    Button(
+        onClick = { /* Do something */ },
+        modifier = modifier.padding(top = 16.dp)
+    ) {
+        Text(text = "Click Me")
+    }
 }
 
 @Preview(showBackground = true)
